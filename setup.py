be4fca3095cdcf224551bfa008db420c18a5f900
Samuel Oliveira <samuelcpoliveira@gmail.com>
2020-11-01 20:13:12 -0300

    Removed SciPy dependency.

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['numpy', 'scipy'],
+    install_requires=['numpy'],
     python_requires='>=3.0'
 )
